e185d85adcc682e7e4e32fcc2808ea1de29ca513
Sultan Majed <sultan.majed@proton.me>
2026-02-16 16:50:59 +0300

    add: method for multipart content type forwarding

--- src/director.rs
+++ src/director.rs
@@ -3,11 +3,11 @@ use axum::{
     http::{Method, HeaderMap, StatusCode},
     response::IntoResponse,
 };
-use reqwest::Client;
+use reqwest::{Client, header, multipart::Form};
 
 use tracing::{debug, info};
 
-pub async fn redirect_to_backend(
+pub async fn forward_json(
     method: Method, 
     target_url: String, 
     headers: HeaderMap,
@@ -15,10 +15,48 @@ pub async fn redirect_to_backend(
     client: Client
 ) -> axum::http::Response<axum::body::Body> {
 
+
     let response = client
         .request(method, &target_url)
         .headers(headers)
+        .header(header::CONTENT_TYPE, "application/json")
         .body(body)
+        .send()
+        .await;
+
+    match response {
+        Ok(res) => {
+            let status = res.status();
+            let headers = res.headers().clone();
+            let body = res.bytes().await.unwrap_or_default();
+
+            info!("sent successfully to ({})", target_url);
+            debug!("{:#?}", &body);
+            info!("response_status = {status:#?}");
+
+            (status, headers, body).into_response()
+        }
+        Err(res) => {
+            info!("{:#?}", res.to_string());
+            info!("{:#?}", StatusCode::BAD_GATEWAY);
+
+            (StatusCode::BAD_GATEWAY, "NestJS is unreachable").into_response()
+        }
+    }
+}
+
+pub async fn forward_multipart(
+    method: Method, 
+    target_url: String, 
+    headers: HeaderMap,
+    form: Form, 
+    client: Client,
+) -> axum::http::Response<axum::body::Body> {
+
+    let response = client
+        .request(method, &target_url)
+        .headers(headers)
+        .multipart(form)
         .send()
         .await;
 
